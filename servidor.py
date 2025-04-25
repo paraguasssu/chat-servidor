39f94df4a3e613a15743c5bf6b3295d4ff08dc17
paraguasssu <rebecapm30@gmail.com>
2025-04-25 18:13:05 -0400

    Add files via upload

---
+++ servidor.py
@@ -0,0 +1,40 @@
+import socket
+import threading
+import os
+
+HOST = '0.0.0.0'
+PORTA = int(os.environ.get("PORT", 10000))  # Render vai definir a porta
+
+clientes = []
+
+def broadcast(mensagem, cliente_atual):
+    for cliente in clientes:
+        if cliente != cliente_atual:
+            try:
+                cliente.send(mensagem)
+            except:
+                cliente.close()
+                clientes.remove(cliente)
+
+def lidar_com_cliente(cliente):
+    while True:
+        try:
+            mensagem = cliente.recv(1024)
+            broadcast(mensagem, cliente)
+        except:
+            clientes.remove(cliente)
+            cliente.close()
+            break
+
+servidor = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+servidor.bind((HOST, PORTA))
+servidor.listen()
+
+print(f"Servidor rodando na porta {PORTA}...")
+
+while True:
+    cliente, endereco = servidor.accept()
+    print(f"Conexão de {endereco}")
+    clientes.append(cliente)
+    thread = threading.Thread(target=lidar_com_cliente, args=(cliente,))
+    thread.start()
